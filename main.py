9f91c10374389315add461fb10d24b1fe726b1c0
Kaueodev <kaue.guimaraes01@gmail.com>
2025-04-06 18:43:46 -0300

    Estrutura final

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os #Biblioteca para funcionamento integrada do py
 
-from organizer import organize_folder #Chama a função na organizer.py
+from organizer import organize_folder, TIPOS_DE_ARQUIVOS #Chama a função na organizer.py
 
 def main():
     print('====Documents Organizer====')             #Apenas uma função inicial para o usuário
-    pasta = input("Digite o caminho da pasta que deseja organizar: ").strip()
+    caminho = input("Digite o caminho da pasta que deseja organizar: ").strip()
 
-    if not os.path.isdir(pasta):
+    if not os.path.exists(caminho):
         print('Caminho inválido, certifique-se de que a pasta existe')  #Se o caminho não funcionar cai nessa condição
         return
 
-    organize_folder(pasta)       #Se o caminho estiver correto, vai cair nessa parte que irá rodar o programa e organizar os docs
+    organize_folder(caminho, TIPOS_DE_ARQUIVOS)   #Se o caminho estiver correto, vai cair nessa parte que irá rodar o programa e organizar os docs
     print('====Organização concluida com sucesso!====')
 
 if __name__ == '__main__':
