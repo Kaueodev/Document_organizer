f577d29246cde55d7b9c0b4a23380da5f933e49e
Kaueodev <kaue.guimaraes01@gmail.com>
2025-04-07 23:23:22 -0300

    Correção erro

--- organizer.py
+++ organizer.py
@@ -23,16 +23,16 @@ def organize_folder(caminho, tipos_dict): #Função que irá receber o caminho d
     for item in os.listdir(caminho): #Percorre todos os itens da pasta indicada
         item_path = os.path.join(caminho, item) #Cria o caminho do item atual
 
-    if os.path.isfile(item_path): #Filtra o que é arquivo(file) ignorando pastas
-        _, extensao = os.path.splitext(item) #Separa a extensao do nome do arquivo
-        categoria = obter_categoria(extensao) #Descobre o tipo do arquivo imagens, videos, documentos, etc...
-        pasta_destino = os.path.join(caminho, categoria) #Irá mover o arquivo para sua determinada pasta
+        if os.path.isfile(item_path): #Filtra o que é arquivo(file) ignorando pastas
+            _, extensao = os.path.splitext(item) #Separa a extensao do nome do arquivo
+            categoria = obter_categoria(extensao) #Descobre o tipo do arquivo imagens, videos, documentos, etc...
+            pasta_destino = os.path.join(caminho, categoria) #Irá mover o arquivo para sua determinada pasta
 
-    if not os.path.exists(pasta_destino):
-        os.makedirs(pasta_destino)   #Essa condição vai criar a pasta caso ela ainda não exista
+            if not os.path.exists(pasta_destino):
+                os.makedirs(pasta_destino)   #Essa condição vai criar a pasta caso ela ainda não exista
 
-    novo_caminho = os.path.join(pasta_destino, item) #Cria o novo caminho completo onde o arquivo vai ficar após ser movido
+            novo_caminho = os.path.join(pasta_destino, item) #Cria o novo caminho completo onde o arquivo vai ficar após ser movido
 
-    shutil.move(item_path, novo_caminho) #Move o arquivo para a pasta correta
+            shutil.move(item_path, novo_caminho) #Move o arquivo para a pasta correta
 
 
